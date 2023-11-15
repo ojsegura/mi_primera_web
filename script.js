1a114adb8b47231fea62bb45f1878362ce6ca83d
ojsegura <oscarsegura91@gmail.com>
2023-11-14 19:40:27 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//alert("Hola este es mi Javascript");
+let nombre = "Oscar";
+
+var nombre1 = "javier";
+
+const nombre2 = "Segura";
+
+console.log(nombre);
+console.log(nombre1);
+console.log(nombre2);
+
+//
